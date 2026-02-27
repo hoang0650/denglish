a2409e7be244dc30638a7277c2f8ad86da22f24a
hoang0650 <hoangphan29091999@gmail.com>
2026-02-27 07:39:50 +0700

    update

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def train():
         load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
+        bnb_4bit_compute_dtype=torch.float16
     )
 
     # 3. Load Model
